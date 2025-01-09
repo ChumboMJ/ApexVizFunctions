51c53bce5a310d13d008094e56551ff647809787
ChumboMJ <tforste@gmail.com>
2025-01-08 16:23:34 -0800

    Create the initial Mutation for uploading a RaceResult to cosmos DB

---
+++ Mutation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace ApexVizFunctions
+{
+    public class Mutation
+    {
+        private readonly Container _container;
+
+        public Mutation(Container container)
+        {
+            _container = container;
+        }
+
+        public async Task<RaceResult> AddRaceResult(RaceResult raceResult)
+        {
+            //TODO: Ensure that the PartitionKey is correctly set
+            var response = await _container.CreateItemAsync(raceResult, new PartitionKey(raceResult.Class));
+            return response.Resource;
+        }
+    }
+}
