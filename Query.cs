1014b986ab7042b088b1b1010d75c65f4387b421
ChumboMJ <tforste@gmail.com>
2025-01-09 14:03:15 -0800

    Remove RaceResult from Query since it was moved to its own file

--- Query.cs
+++ Query.cs
@@ -1,3 +1,6 @@
+using HotChocolate;
+using HotChocolate.Types.Relay;
+
 namespace ApexVizFunctions;
 
 public class Query
@@ -13,19 +16,4 @@ public class Person
     }
 
     public string Name { get; }
-}
-
-public class RaceResult
-{
-    public int PaxPosition { get; set; }
-    public int ClassPosition { get; set; }
-    public string Class { get; set; }
-    public int Number { get; set; }
-    public string Driver { get; set; }
-    public string Car { get; set; }
-    public decimal Total { get; set; }
-    public decimal Factor { get; set; }
-    public decimal PaxTime { get; set; }
-    public decimal Diff { get; set; }
-    public decimal FromFirst { get; set; }
 }
