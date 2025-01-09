5003bcc809bbf4c699304e2b4de5cc92059ab5f6
ChumboMJ <tforste@gmail.com>
2025-01-08 22:18:41 -0800

    Create new options for ApexVizFunctions

---
+++ ApexVizFunctionsOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace ApexVizFunctions
+{
+    public class ApexVizFunctionsOptions
+    {
+        public const string ApexVixFunctions = "ApexVizFunctions";
+        public CosmosDbOptions CosmosDb { get; set; }
+
+        public class CosmosDbOptions
+        {
+            public string ConnectionString { get; set; }
+            public string DatabaseName { get; set; }
+        }
+    }
+}
