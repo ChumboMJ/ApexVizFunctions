6e356489edbdb8e3f08b5b4a028d54e936325fd1
ChumboMJ <tforste@gmail.com>
2025-01-09 14:30:04 -0800

    Create a ResultType class to represent the different types fo results that can be uploaded

---
+++ Models/ResultType.cs
@@ -0,0 +1,9 @@
+﻿namespace ApexVizFunctions.Models
+{
+    public class ResultType
+    {
+        public const string Pax = "PAX";
+        public const string Raw = "RAW";
+        public const string Lap = "LAP";
+    }
+}
